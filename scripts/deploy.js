53dae3977365d5fbfbdbd3a0e077df5d5138f2c1
sebaszeledon <sebas.zeledon@gmail.com>
2024-04-02 20:19:48 -0600

    First verification

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,51 @@
-const {ethers} = require("hardhat"); //8:41:42
+const {ethers, run, network} = require("hardhat"); //8:41:42
+
+//Install prettier: 
 
 async function main(){
+
     const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
     console.log("Deploying contract...");
     const simpleStorage = await SimpleStorageFactory.deploy();
     await simpleStorage.getDeployedCode();
-    console.log(`Deployed contract to: ${simpleStorage.target}`)
+    console.log(`Deployed contract to: ${simpleStorage.target}`);
+
+    if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+        await simpleStorage.deploymentTransaction().wait(6);
+        await verify(simpleStorage.target, []);
+    }
+
+    const currentValue = await simpleStorage.retrieve();
+    console.log(`Current Value is: ${currentValue}`);
+
+    // Update the current value
+    const transactionResponse = await simpleStorage.store(8);
+    await transactionResponse.wait(1);
+    const updatedValue = await simpleStorage.retrieve();
+    console.log(`Updated value is: ${updatedValue}`);
+
+}
+
+async function verify(contractAddress, args) {
+    console.log("Verifying contract...");
+    try {
+    await run("verify:verify", {
+        address: contractAddress,
+        constructorArguments: args,
+    });
+    } catch (e){
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified!");
+        } else {
+            console.log(e);
+        }
+    }
 }
 
 main()
     .then(() => process.exit(0))
     .catch((error) => {
-        console.error(error)
-        process.exit(1)
-    })
+        console.error(error);
+        process.exit(1);
+    });
 
